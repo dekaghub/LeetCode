b5b9fd96a0174ace348be5efa107a74dbf6cc435
Susanta <sdeka@bgsu.edu>
2020-02-06 09:38:04 -0500

    dict + faster code

--- AddTwoProblem.py
+++ AddTwoProblem.py
@@ -12,27 +12,43 @@ class Solution(object):
         :rtype: ListNode
         """
         
-        n = m = ''
-        
-        x = l1
-        while x.next:
-            n += str(x.val)
-            x = x.next
-        n += str(x.val)
-        
-        x = l2
-        while x.next:
-            m += str(x.val)
-            x = x.next
-        m += str(x.val)
-        
-        result = str(int(m[::-1]) + int(n[::-1]))
+        d = {}
 
-        node = ListNode(int(result[-1]))
+        p = l1
+        count = 0
+        dkey = 1
+        while p.next:
+          d[dkey] = (10**count)*p.val
+          count += 1
+          dkey += 1
+          p = p.next
+
+        d[dkey] = (10**count)*p.val
+
+        count = 0
+        dkey += 1
+        p = l2
+        while p.next:
+          d[dkey] = (10**count)*p.val
+          count += 1
+          dkey += 1
+          p = p.next
+
+        d[dkey] = (10**count)*p.val
+
+        result = sum(d.values())
+
+        result_list = [i for i in str(result)]
+
+        # Alternate method for single digit extraction
+        # import math
+        # result_list = [(ret//(10**i))%10 for i in range(math.ceil(math.log(ret, 10))-1, -1, -1)]
+
+        node = ListNode(result_list[-1])
         curr = node
         
-        for i in range(len(result)-2, -1, -1):
-            curr.next = ListNode(int(result[i]))
+        for i in range(len(result_list)-2, -1, -1):
+            curr.next = ListNode(result_list[i])
             curr = curr.next
             
         curr.next = None
